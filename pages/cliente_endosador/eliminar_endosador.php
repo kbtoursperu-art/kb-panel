5d78510af6c8bb07638528ce9520d053755ea801
Anais <anaiszunigaastra@gmail.com>
2026-01-20 13:44:27 -0500

    eliminar funcional 3

--- pages/cliente_endosador/eliminar_endosador.php
+++ pages/cliente_endosador/eliminar_endosador.php
@@ -1,28 +1,11 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 include '../../conexion.php';
 
-if (isset($_POST['id_cliente']) && is_numeric($_POST['id_cliente'])) {
+if (isset($_GET['id_cliente']) && is_numeric($_GET['id_cliente'])) {
 
-    $id_cliente = (int) $_POST['id_cliente'];
+    $id_cliente = (int) $_GET['id_cliente'];
 
-    // Obtener foto si existe
-    $query_foto = mysqli_query($conexion, "
-        SELECT foto_documento 
-        FROM Clientes_Endosadores 
-        WHERE id_cliente = $id_cliente
-    ");
-    $row = mysqli_fetch_assoc($query_foto);
-
-    if ($row && !empty($row['foto_documento'])) {
-        $ruta_foto = 'uploads/' . $row['foto_documento'];
-        if (file_exists($ruta_foto)) {
-            unlink($ruta_foto);
-        }
-    }
-
-    // Eliminar cliente (CASCADE elimina en Clientes_Endosadores)
+    // Eliminar cliente (ON DELETE CASCADE eliminará Clientes_Endosadores)
     $delete = mysqli_query(
         $conexion,
         "DELETE FROM Datos_clientes WHERE id_cliente = $id_cliente"
@@ -31,18 +14,19 @@ if (isset($_POST['id_cliente']) && is_numeric($_POST['id_cliente'])) {
     if ($delete) {
         echo "<script>
             alert('Cliente Endosador eliminado correctamente');
-            window.location='clientes_endosadores.php';
+            window.location='endosadores.php';
         </script>";
     } else {
         echo "<script>
             alert('Error al eliminar el cliente');
-            window.location='clientes_endosadores.php';
+            window.location='endosadores.php';
         </script>";
     }
 
 } else {
     echo "<script>
         alert('ID inválido');
-        window.location='clientes_endosadores.php';
+        window.location='endosadores.php';
     </script>";
 }
+?>
