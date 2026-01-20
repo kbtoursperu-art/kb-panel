99d3079afbbadbfead302815376d6b4c7ae39323
Anais <anaiszunigaastra@gmail.com>
2026-01-20 13:47:43 -0500

    eliminar funcional 4

--- pages/cliente_endosador/eliminar_endosador.php
+++ pages/cliente_endosador/eliminar_endosador.php
@@ -1,31 +1,29 @@
 <?php
 include '../../conexion.php';
 
-if (isset($_GET['id_cliente']) && is_numeric($_GET['id_cliente'])) {
-
-    $id_cliente = (int) $_GET['id_cliente'];
-
-    // Eliminar cliente (ON DELETE CASCADE eliminará Clientes_Endosadores)
-    $delete = mysqli_query(
-        $conexion,
-        "DELETE FROM Datos_clientes WHERE id_cliente = $id_cliente"
-    );
-
-    if ($delete) {
-        echo "<script>
-            alert('Cliente Endosador eliminado correctamente');
-            window.location='endosadores.php';
-        </script>";
-    } else {
-        echo "<script>
-            alert('Error al eliminar el cliente');
-            window.location='endosadores.php';
-        </script>";
-    }
-
-} else {
+if (!isset($_GET['id_cliente']) || !is_numeric($_GET['id_cliente'])) {
     echo "<script>
         alert('ID inválido');
+        window.location='endosadores.php';
+    </script>";
+    exit;
+}
+
+$id_cliente = (int) $_GET['id_cliente'];
+
+$delete = mysqli_query(
+    $conexion,
+    "DELETE FROM Datos_clientes WHERE id_cliente = $id_cliente"
+);
+
+if ($delete) {
+    echo "<script>
+        alert('Cliente Endosador eliminado correctamente');
+        window.location='endosadores.php';
+    </script>";
+} else {
+    echo "<script>
+        alert('Error al eliminar el cliente');
         window.location='endosadores.php';
     </script>";
 }
